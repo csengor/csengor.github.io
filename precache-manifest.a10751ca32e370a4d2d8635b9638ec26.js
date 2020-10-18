f12bd42bd0066805f445584a579c97058eac8667
Çağatay Onur Şengör <contact@csengor.com>
2020-10-18 23:13:13 +0300

    Rewrite page in reactjs

---
+++ precache-manifest.a10751ca32e370a4d2d8635b9638ec26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a85fb66513b3d9071a2e08a630e4ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8634205225d6a757a60e",
+    "url": "/static/css/main.a5211489.chunk.css"
+  },
+  {
+    "revision": "1fc4e8a99eac20013030",
+    "url": "/static/js/2.0c921dd3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0c921dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8634205225d6a757a60e",
+    "url": "/static/js/main.436c6dc3.chunk.js"
+  },
+  {
+    "revision": "e939f0fb24941518c4dc",
+    "url": "/static/js/runtime-main.81defe3e.js"
+  }
+]);
